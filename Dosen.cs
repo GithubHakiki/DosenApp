d55cc704183d7f0733ebf2a794ccfd98a45b5dc7
GithubHakiki <127917441+GithubHakiki@users.noreply.github.com>
2023-05-15 23:52:48 +0700

    Add files via upload

---
+++ Dosen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DosenApp
+{
+    public class Dosen
+    {
+        public string Matakuliah { get; set; }
+        public string Nama { get; set; }
+        public int Nik { get; set; }
+
+        public Dosen()
+        {
+            // constructor untuk mengeset nik dan nama dosen pengampu matakuliah Pemrograman
+            this.Nik = 05250xxxxx;
+            this.Nama = "Kamarudin, M.Kom";
+            this.Matakuliah = "Pemrograman";
+        }
+
+        public void SetDosen(int nik, string nama)
+        {
+            // method input nama dosen dan niknya
+            this.Nik = nik;
+            this.Nama = nama;
+        }
+
+        public void SetMatakuliah(string matakuliah)
+        {
+            // method input nama matakuliah
+            this.Matakuliah = matakuliah;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using DosenApp;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dosen dosen = new Dosen();
+
+        Console.WriteLine("NIK: {0}", dosen.Nik);
+        Console.WriteLine("Nama: {0}", dosen.Nama);
+        Console.WriteLine("Mata Kuliah: {0}", dosen.Matakuliah);
+
+        Console.WriteLine("\nInput NIK Dosen:");
+        int nik = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Input Nama Dosen:");
+        string nama = Console.ReadLine();
+
+        dosen.SetDosen(nik, nama);
+
+        Console.WriteLine("Input Matakuliah:");
+        string matakuliah = Console.ReadLine();
+
+        dosen.SetMatakuliah(matakuliah);
+
+        Console.WriteLine("\nNIK: {0}", dosen.Nik);
+        Console.WriteLine("Nama: {0}", dosen.Nama);
+        Console.WriteLine("Matakuliah: {0}", dosen.Matakuliah);
+
+        Console.ReadKey();
+    }
+}
